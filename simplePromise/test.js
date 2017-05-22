571c878f37ff3b97cf4f7f661d883f0eeb8b90cb
Gurpinder Singh Sandhu <gpsinghsandhu@yahoo.com>
2017-05-22 16:30:18 +0530

    replaced all vars with let in tests

--- simplePromise/test.js
+++ simplePromise/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var chai = require('chai'),
+let chai = require('chai'),
     assert = chai.assert,
     MyPromise = require('./promise.js'),
     chaiAsPromised = require('chai-as-promised'),
@@ -12,40 +12,40 @@ chai.use(chaiAsPromised);
 describe('Promise constructor', function() {
 
     it('should return something like promise (i.e object with then method)', function() {
-        var promise = new MyPromise(function(reject, resolve) {});
+        let promise = new MyPromise(function(reject, resolve) {});
 
         assert.isObject(promise, 'promise is object');
         assert.isFunction(promise.then, 'promise has a then method');
     });
 
     it('should return a promise that resolves', function() {
-        var promise = new MyPromise(function(resolve, reject) { setTimeout(() => resolve('val'), 10); });
+        let promise = new MyPromise(function(resolve, reject) { setTimeout(() => resolve('val'), 10); });
 
         return assert.isFulfilled(promise, 'promise should fulfill');
     });
 
     it('should return a promise that rejects', function() {
-        var promise = new MyPromise(function(resolve, reject) { setTimeout(() => reject('val'), 10); });
+        let promise = new MyPromise(function(resolve, reject) { setTimeout(() => reject('val'), 10); });
 
         return assert.isRejected(promise, 'val', 'promise should reject');
     });
 
     describe('fn is passed to constructor', function() {
         it('should return rejected promise if passed fn throws', function() {
-            var err = new Error('error'),
+            let err = new Error('error'),
                 promise = new MyPromise((resolve, reject) => {throw err;});
 
             return assert.isRejected(promise, 'error', 'promise should reject');
         });
 
         it('should ignore thrown error if resolved called first', function() {
-            var promise = new MyPromise((resolve, reject) => { resolve('some'); throw new Error('error'); });
+            let promise = new MyPromise((resolve, reject) => { resolve('some'); throw new Error('error'); });
 
             return assert.becomes(promise, 'some', 'thrown error not ignored');
         });
 
         it('should ignore thrown error if reject called first', function() {
-            var promise = new MyPromise((resolve, reject) => { reject('some'); throw new Error('error'); });
+            let promise = new MyPromise((resolve, reject) => { reject('some'); throw new Error('error'); });
 
             return assert.isRejected(promise, 'some', 'thrown error not ignored');
         });
